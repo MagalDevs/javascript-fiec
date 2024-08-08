9833f9b79f5a4621e0921994ce49ffb8e8af8dcc
DANIEL GALVAO MAGALHAES DA SILVA <1051392411001@fatec>
2024-08-08 10:37:44 -0300

    adicionando

--- atividades/atividade 07-08-24/atividade1.js
+++ atividades/atividade 07-08-24/atividade1.js
@@ -1,30 +1,30 @@
 function main(){
     let numero = parseInt((prompt("Digite um número(digite 999 para parar)")));
     console.log(numero);
-    let maior = numero
-    let menor = numero
+    let maior = numero;
+    let menor = numero;
     if(numero > 999 || numero < 1){
-        alert("Número inválido")
+        alert("Número inválido");
     } else {
-        soma = numero
-        contador = 1
+        soma = numero;
+        contador = 1;
         while(numero != 999 && numero >= 1 && numero < 999){
             let numero = parseInt((prompt("Digite um número(digite 999 para parar)")));
             console.log(numero);
             if (numero>999 || numero<1){
-                alert("Número inválido")
+                alert("Número inválido");
                 break
             } else if (numero == 999){
-                let divisao = soma / contador
-                alert(`Maior número: ${maior}\nMenor número: ${menor}\nSoma: ${soma}\nMédia: ${divisao}`)
+                let divisao = soma / contador;
+                alert(`Maior número: ${maior}\nMenor número: ${menor}\nSoma: ${soma}\nMédia: ${divisao}`);
                 break
             } else{
-                soma = numero + soma
-                contador = 1 + contador
+                soma = numero + soma;
+                contador = 1 + contador;
                 if(numero > maior){
-                    maior = numero
+                    maior = numero;
                 }else if(numero < menor){
-                    menor = numero
+                    menor = numero;
                 }
     
             }
