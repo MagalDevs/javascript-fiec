88c3021e93560f2ad9a663dc1d109462a9478bad
unknown <37046@fiecalunos.lan>
2024-08-21 22:47:48 -0300

    aula-21-08-24 parte1

---
+++ aulas/aula-21-08-24/exemplo1.js
@@ -0,0 +1,56 @@
+class Automovel{
+    #marca;
+    #modelo;
+    #valor;
+    #cor;
+    #placa
+    #rodas;
+
+    get marca() { return this.#marca}
+
+    set marca(marca) { this.#marca = marca}
+
+    get modelo() { return this.#modelo}
+
+    set modelo(modelo) { this.#modelo = modelo}
+
+    get valor() { return this.#valor}
+
+    set valor(valor) { this.#valor = valor}
+
+    get cor() { return this.#cor}
+
+    set cor(cor) { this.#cor = cor}
+
+    get placa() { return this.#placa}
+
+    set placa(placa) { this.#placa = placa}
+
+    get rodas() { return this.#rodas}
+
+    set marca(rodas) { this.#rodas = rodas}
+
+    constructor(marca, modelo, valor, cor, placa, rodas){
+        this.#marca = marca;
+        this.#modelo = modelo;
+        this.#valor = valor;
+        this.#cor = cor;
+        this.#placa = placa;
+        this.#rodas = rodas;
+
+    }
+}
+
+class Carro extends Automovel{
+    #portas;
+
+    get portas() { return this.#portas}
+
+    set portas(portas) {this.#portas = portas}
+
+    constructor( marca, modelo, valor, cor, placa, rodas, portas){
+        super(marca, modelo, valor, cor, placa, rodas);
+        this.#portas = portas;
+    }
+
+}
