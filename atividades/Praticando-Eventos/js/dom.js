c691413ea1ced052e730d6bc54c06f896f1b78a6
unknown <37046@fiecalunos.lan>
2024-09-11 22:42:59 -0300

    atv-eventos

---
+++ atividades/Praticando-Eventos/js/dom.js
@@ -0,0 +1,22 @@
+async function changeBackgroundColor(element, color) {
+    if (color == "blue") {
+        element.style.backgroundColor = "blue"
+    }else if (color == "green") {
+        element.style.backgroundColor = "green"
+    }
+}
+
+const title = document.getElementById("title");
+const rodape = document.querySelector("footer");
+
+const btnBlue = document.getElementById("Blue")
+const btnGreen = document.getElementById("Green")
+
+btnBlue.addEventListener("click",function(){
+    changeBackgroundColor(title, "blue")
+    changeBackgroundColor(rodape, "blue")
+})
+btnGreen.addEventListener("click",function(){
+    changeBackgroundColor(title, "green")
+    changeBackgroundColor(rodape, "green")
+})
