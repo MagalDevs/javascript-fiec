d6d21c52e9aa29770ff6704243448a9dbb77cb6a
MagalDevs <danielgalfla12@gmail.com>
2024-09-23 19:54:48 -0300

    Aula QR code concluída

--- aulas/QrCode/js/scripts.js
+++ aulas/QrCode/js/scripts.js
@@ -5,6 +5,18 @@ const input = document.querySelector("#qr-form input");
 const imgQrCode = document.querySelector("#qr-code img")
 
 button.addEventListener("click", generateQrCode);
+input.addEventListener("keydown", (event) => {
+    if (event.code === "Enter") {
+        generateQrCode();
+    }
+});
+
+input.addEventListener("keyup", () => {
+    if (!input.value) {
+        container.classList.remove("active");
+        button.innerText = "Gerar QR Code";
+    }
+})
 
 function generateQrCode() {
     const inputValue = input.value;
@@ -14,6 +26,9 @@ function generateQrCode() {
     }
 
     button.innerText = "Gerando o QR code...";
-    imgQrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
-    container.classList.add("active")
+    imgQrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`; 
+    imgQrCode.addEventListener("load", () => {
+        container.classList.add("active")
+        button.innerText = "QR code gerado com sucesso!"
+    });
 }
