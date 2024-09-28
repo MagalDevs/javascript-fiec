292401f7e7d0c4a6c00665247a490efb0ad103a6
MagalDevs <danielgalfla12@gmail.com>
2024-09-28 16:48:33 -0300

    ajustes

--- aulas/aula-12-08-2024/javadoscript.js
+++ aulas/aula-12-08-2024/javadoscript.js
@@ -19,15 +19,6 @@ class Carro {
             this.acelerar = (velocidadeAcelerar) => console.log(`acelerando o carro até: ${velocidadeAcelerar}`),
             this.frear = (velocidadeFrear) => console.log(`acelerando o carro até: ${velocidadeFrear}`);
     }
-    constructor(roda, embreagem, marca, modelo) {
-        this.cor = cor,
-            this.roda = roda,
-            this.embreagem = embreagem,
-            this.marca = marca,
-            this.modelo = modelo,
-            this.acelerar = (velocidadeAcelerar) => console.log(`acelerando o carro até: ${velocidadeAcelerar}`),
-            this.frear = (velocidadeFrear) => console.log(`acelerando o carro até: ${velocidadeFrear}`);
-    }
 }
 
 let ferrari = new Carro("Amarelo", 2, "Manual", "Ferrari", "Dino")
@@ -35,5 +26,5 @@ let ferrari = new Carro("Amarelo", 2, "Manual", "Ferrari", "Dino")
 console.log(ferrari);
 console.log(typeof ferrari);
 console.log(`A cor do a carro é ${ferrari.cor}`);
-carro.acelerar(60);
-carro.frear(30);
+ferrari.acelerar(60);
+ferrari.frear(30);
