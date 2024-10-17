4d6cc50f05b7820a679536931b888281d1b4e999
MagalDevs <danielgalfla12@gmail.com>
2024-10-16 22:55:13 -0300

    p1 concluido

--- aulas/headers-axios/js/index.js
+++ aulas/headers-axios/js/index.js
@@ -17,15 +17,37 @@ const getData = async () => {
 
 getData();
 
+const validCpf = async(cpf) => {
+    try {
+        let url = "https://api.invertexto.com/v1/validator?token=15772|28zq9TbjIuNI224TKotvlBvDIFEocgT4&value="
+        url = url.concat(cpf);
+        const response = await baseAxios.get(url) 
+        console.log(response);
+
+        if(response.data.valid){
+            alert("CPF Válido!");
+        }else{
+            alert("CPF Inválido!");
+        }
+
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
 const form = document.querySelector("#postForm");
 const titleInput = document.querySelector("#title");
 const bodyInput = document.querySelector("#body");
+const validarCpf = document.querySelector("#validarCpf");
+const limparCpf = document.querySelector("#limparCpf");
+const cpf = document.querySelector("#cpf");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     try {
-        axios.post("https://jsonplaceholder.typicode.com/posts",
+        axios.post("/posts",
         {
             title: titleInput.value,
             body: bodyInput.value,
@@ -36,3 +58,16 @@ form.addEventListener("submit", (event) => {
         console.log(error);
     }
 })
+
+//EVENTOS   
+limparCpf.addEventListener("click", (event) => {
+    event.preventDefault();
+    cpf.value = "";
+})
+
+validarCpf.addEventListener("click", (event) => {  
+    event.preventDefault();
+    validCpf(cpf.value);
+}) 
+
+//15772|28zq9TbjIuNI224TKotvlBvDIFEocgT4
